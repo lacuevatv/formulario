6db0bf52f0a97146bc18c2472af0ec2120d8dd5b
coco <coco@lacueva.tv>
2019-04-12 17:53:58 -0300

    creado el script de las pantallas e indice del formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,15 @@
 >>> TABLE OF CONTENTS:
 1.0 DOC READY
 2.0 FUNCIONES
-2.1 initFormularios: da inicio al formulario
+2.1 funciones de limpieza der caracteres y demas
+2.2 initFormulario: da inicio al formulario
+2.3. setea e imprime el indice del formulario
+2.4. 
 --------------------------------------------------------------*/
-
 var baseUrl = 'http://' + window.location.host;
-var ajaxFileUrl = baseUrl + '/inc/ajax.php';
-var paginaIndex = 1;
-var pageActual = $('body').attr('data-page');
-var windowWidth;
-//se pasa con numeral #page
-function scrollToID ( id ) {
-    $('html, body').stop().animate({
-        scrollTop: $(id).offset().top -90
-    }, 'slow');
-}
+//var ajaxFileUrl = baseUrl + '/inc/ajax.php';
+var indexactual = 1;
+
 
 /*--------------------------------------------------------------
 1.0 BASE 
@@ -31,28 +26,31 @@ $(document).ready(function(){
 
 });//.ready()
 
+
 /*--------------------------------------------------------------
 2.0 FUNCIONES
 --------------------------------------------------------------*/
-function initFormularios(){
-    var indexactual = 0;
-    setIndex(indexactual);
-    var specialcharacters = '@#$^&%*()+=[]\'\"\/{}|:;¡!¿?<>,.';
-    var numeros = '0123456789';
-    var letras = 'abcdefghijklmnñopqrstuvwxyz';
-
-    //busca los caracteres indicados en un string y devuelve true si existen
-    function areThereAny ( cadena, characters ) {
-        for (var i = 0; i < characters.length; i++) {
+//se pasa con numeral #page
+function scrollToID ( id ) {
+    $('html, body').stop().animate({
+        scrollTop: $(id).offset().top -90
+    }, 'slow');
+}
+/*
+ * funciones de limpieza
+*/
+//busca los caracteres indicados en un string y devuelve true si existen
+function areThereAny( cadena, characters ) {
+    for (var i = 0; i < characters.length; i++) {
         if ( cadena.indexOf(characters[i]) != -1 ) {
                 return true;    
         }
     }
     return false;
-    }
+}
 
-    //quita numeros de un string
-    function cleanedOthers(cadena, caracteres){ 
+//quita caracteres especificos a un string
+function cleanedOthers(cadena, caracteres){ 
 
     //eliminamos uno por uno
     for (var i = 0; i < caracteres.length; i++) {
@@ -60,10 +58,11 @@ function initFormularios(){
     }   
 
     return cadena;
-    }
+}
 
-    //quita caracteres extraños del string, los caracteres se pasan como una variable
-    function cleanedSpecialCharacters(cadena, specialcharacters){ 
+//quita caracteres extraños del string, los caracteres se pasan como una variable
+function cleanedSpecialCharacters( cadena ){ 
+    var specialcharacters = '@#$^&%*()+=[]\'\"\/{}|:;¡!¿?<>,.';
 
     //eliminamos uno por uno
     for (var i = 0; i < specialcharacters.length; i++) {
@@ -71,69 +70,129 @@ function initFormularios(){
     }   
 
     return cadena;
-    }
+}
 
-    //lo pasa a minúsculas
-    function toLowerCase(cadena) {
-        cadena = cadena.toLowerCase();
-        return cadena;
-    }
+//lo pasa a minúsculas
+function toLowerCase(cadena) {
+    cadena = cadena.toLowerCase();
+    return cadena;
+}
 
-    //remplasa dashes "-" del string por espacios
-    function replaceDashes( cadena ) {
+//remplasa dashes "-" del string por espacios
+function replaceDashes( cadena ) {
     cadena = cadena.replace(/-/gi," ");
     cadena = cadena.replace(/_/gi," ");
     return cadena;
-    }
+}
 
 
-    //borra espacios del string
-    function removeDashesSpaces( cadena ) {
+//borra espacios del string
+function removeDashesSpaces( cadena ) {
     cadena = cadena.replace(/-/gi,"");
     cadena = cadena.replace(/_/gi,"");
     cadena = cadena.replace(/ /gi,"");
     return cadena;
-    }
-
-    // Quitamos espacios y los sustituimos por - porque nos gusta mas asi
-    function replaceSpaces( cadena ) {
-	    cadena = cadena.replace(/ /gi,"-");
-	    return cadena;
-    }
-
-    //quita acentos y ñ y lo pasa a minúsculas
-    function cleanAcentos( cadena ) {
-
-        // Lo queremos devolver limpio en minusculas
-        cadena = cadena.toLowerCase();
-
-        // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
-        cadena = cadena.replace(/á/gi,"a");
-        cadena = cadena.replace(/é/gi,"e");
-        cadena = cadena.replace(/í/gi,"i");
-        cadena = cadena.replace(/ó/gi,"o");
-        cadena = cadena.replace(/ú/gi,"u");
-        cadena = cadena.replace(/ñ/gi,"n");
-        return cadena;
-    }
-
 }
+
+// Quitamos espacios y los sustituimos por - porque nos gusta mas asi
+function replaceSpaces( cadena ) {
+    cadena = cadena.replace(/ /gi,"-");
+    return cadena;
+}
+
+//quita acentos y ñ y lo pasa a minúsculas
+function cleanAcentos( cadena ) {
+
+    // Lo queremos devolver limpio en minusculas
+    cadena = cadena.toLowerCase();
+
+    // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+    cadena = cadena.replace(/á/gi,"a");
+    cadena = cadena.replace(/é/gi,"e");
+    cadena = cadena.replace(/í/gi,"i");
+    cadena = cadena.replace(/ó/gi,"o");
+    cadena = cadena.replace(/ú/gi,"u");
+    cadena = cadena.replace(/ñ/gi,"n");
+    return cadena;
+}
+
+/*
+ * FUNCIONES DEL PROGRAMA
+*/
+
+function initFormularios(){
+   
+    //coloca la primer pantalla para empezar
+    setScreenFormulario(indexactual);
+
+}//initFormularios()
+
+
+//va navegando por las pantallas del formulario
+function setScreenFormulario( index ) {
+    console.log(index, indexactual);
+    var contenedorPantallas = $('.pantallas-formulario');
+    var pantallas = $('.pantalla');
+    
+    $(pantallas[index-2]).removeClass('activa');
+    $(pantallas[index-1]).addClass('activa');
+    $(pantallas[index]).removeClass('activa');
+
+
+    if ( indexactual >= 3) {
+        setIndex(indexactual);
+    }
+
+    indexactual = index+1;
+}
+
 
 //carga o modifica el indice de acuerdo al index actual
-function setIndex(indexactual) {
-    var indiceTotal = 3;
-    var contenedor = $('.index-wrapper');
-    var html = '<ul class="dots">';
-    for (var i = 0; i < indiceTotal; i++) {
+function setIndex( index ) {
+    var indices = $('.index-wrapper .dots li');
+
+    //si no esta cargado lo carga y pone por defecto el
+    if ( !( indices.length > 0 ) ) {
+        var indiceTotal = 3
+        var html = '';
+        for (var i = 0; i < indiceTotal; i++) {
+            
+            html += '<li';
+            /*if ( i == 0 ) {
+                html += ' class="active active-1"';
+            }*/
+            html += '><span class="number-index">'+(i+1)+'</span></li>';
+            
+        }//for
         
-        html += '<li';
-        if ( i == 0 ) {
-            html += ' class="active active-1"';
-        }
-        html += '><span class="number-index">'+(i+1)+'</span></li>';
-        
-    }//for
-    
-    html += '</ul>';
-    contenedor.append( $(html) );
-}
+        $('.index-wrapper .dots').append( $(html) );
+    }
+
+    //ahora se setea el indice
+    switch (index) {
+        case 3:
+            var indices = $('.index-wrapper .dots li');
+            $(indices[0]).addClass('active active-1');
+        break;
+        case 4:
+            $(indices[0]).addClass('active active-2');
+        break;
+        case 5:
+            $(indices[0]).addClass('active active-full');
+            $(indices[1]).addClass('active');
+        break;
+        case 6:
+            $(indices[1]).addClass('active-1');
+        break;
+        case 7:
+            $(indices[1]).addClass('active-2');
+        break;
+        case 8:
+            $(indices[1]).addClass('active-3');
+        break;
+        case 9:
+            $(indices[1]).addClass('active-full');
+            $(indices[2]).addClass('active');
+        break;
+    }
+}//setIndex()
