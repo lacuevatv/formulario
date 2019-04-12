8ff9afd5b9f8c28fb87a4d116a6161decf273fb6
coco <coco@lacueva.tv>
2019-04-12 09:22:28 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,119 @@
+/**
+ * File script.js
+ *
+ * @required jQuery
+ * @ver 1.0
+ --------------------------------------------------------------
+>>> TABLE OF CONTENTS:
+1.0 DOC READY
+2.0 FUNCIONES
+2.1 initFormularios: da inicio al formulario
+--------------------------------------------------------------*/
+
+var baseUrl = 'http://' + window.location.host;
+var ajaxFileUrl = baseUrl + '/inc/ajax.php';
+var paginaIndex = 1;
+var pageActual = $('body').attr('data-page');
+var windowWidth;
+//se pasa con numeral #page
+function scrollToID ( id ) {
+    $('html, body').stop().animate({
+        scrollTop: $(id).offset().top -90
+    }, 'slow');
+}
+
+/*--------------------------------------------------------------
+1.0 BASE 
+* navigation, scroll to
+--------------------------------------------------------------*/
+$(document).ready(function(){
+    initFormularios();
+
+});//.ready()
+
+/*--------------------------------------------------------------
+2.0 FUNCIONES
+--------------------------------------------------------------*/
+function initFormularios(){
+
+    var specialcharacters = '@#$^&%*()+=[]\'\"\/{}|:;¡!¿?<>,.';
+    var numeros = '0123456789';
+    var letras = 'abcdefghijklmnñopqrstuvwxyz';
+
+    //busca los caracteres indicados en un string y devuelve true si existen
+    function areThereAny ( cadena, characters ) {
+        for (var i = 0; i < characters.length; i++) {
+        if ( cadena.indexOf(characters[i]) != -1 ) {
+                return true;    
+        }
+    }
+    return false;
+    }
+
+    //quita numeros de un string
+    function cleanedOthers(cadena, caracteres){ 
+
+    //eliminamos uno por uno
+    for (var i = 0; i < caracteres.length; i++) {
+        cadena= cadena.replace(new RegExp(caracteres[i], 'gi'), '');
+    }   
+
+    return cadena;
+    }
+
+    //quita caracteres extraños del string, los caracteres se pasan como una variable
+    function cleanedSpecialCharacters(cadena, specialcharacters){ 
+
+    //eliminamos uno por uno
+    for (var i = 0; i < specialcharacters.length; i++) {
+        cadena= cadena.replace(new RegExp("\\" + specialcharacters[i], 'gi'), '');
+    }   
+
+    return cadena;
+    }
+
+    //lo pasa a minúsculas
+    function toLowerCase(cadena) {
+        cadena = cadena.toLowerCase();
+        return cadena;
+    }
+
+    //remplasa dashes "-" del string por espacios
+    function replaceDashes( cadena ) {
+    cadena = cadena.replace(/-/gi," ");
+    cadena = cadena.replace(/_/gi," ");
+    return cadena;
+    }
+
+
+    //borra espacios del string
+    function removeDashesSpaces( cadena ) {
+    cadena = cadena.replace(/-/gi,"");
+    cadena = cadena.replace(/_/gi,"");
+    cadena = cadena.replace(/ /gi,"");
+    return cadena;
+    }
+
+    // Quitamos espacios y los sustituimos por - porque nos gusta mas asi
+    function replaceSpaces( cadena ) {
+	    cadena = cadena.replace(/ /gi,"-");
+	    return cadena;
+    }
+
+    //quita acentos y ñ y lo pasa a minúsculas
+    function cleanAcentos( cadena ) {
+
+        // Lo queremos devolver limpio en minusculas
+        cadena = cadena.toLowerCase();
+
+        // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+        cadena = cadena.replace(/á/gi,"a");
+        cadena = cadena.replace(/é/gi,"e");
+        cadena = cadena.replace(/í/gi,"i");
+        cadena = cadena.replace(/ó/gi,"o");
+        cadena = cadena.replace(/ú/gi,"u");
+        cadena = cadena.replace(/ñ/gi,"n");
+        return cadena;
+    }
+
+}
